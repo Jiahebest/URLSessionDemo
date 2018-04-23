72facc57fbe4b72e84e1c7fca00ff8593cdc5c75
gdcp <gdcp@m25.local>
2018-04-23 11:06:21 +0800

    add

--- URLSessionDemo/ViewController.swift
+++ URLSessionDemo/ViewController.swift
@@ -12,7 +12,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //1、根据URL字符串创建一个URL实例
+        let url = URL(string: "http://a1.gdcp.cn")
+        //2、根据创建的URL实例，创建一个URLRequest实例（要求为默认值）
+        let request = URLRequest(url: url!)
+        //3、创建URLSession会话实例 可以使用URLSession.shared返回一个共享的会话，该会话使用全局的Cache、Cookie和证书
+        //4、使用创建好的URLSession实例和URLrequset实例创建一个URLSessionTask
+        let dataTask = URLSession.shared.dataTask(with: request){data,responese,error in
+            if error != nil {
+            print(error.debugDescription)
+            }else{
+                let str = String(data: data!, encoding: String.Encoding.ascii)
+                print(str!)
+                
+            }
+        }as URLSessionTask
+        dataTask.resume()
     }
 
     override func didReceiveMemoryWarning() {
